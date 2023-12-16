416653707874a0d1acf16a45b8339dfd46012082
Juancho-Coding <juanpabloalvarezovalle@gmail.com>
2023-12-16 15:46:49 -0500

    Changed develop tools, added theme to enter point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+
+const theme = createTheme({
+    typography: { fontFamily: "Roboto Slab, serif", allVariants: { color: "white" } },
+});
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);
