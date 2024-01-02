265e6960af24d2130dbf402260cbf6e422d4feec
Juancho-Coding <juanpabloalvarezovalle@gmail.com>
2024-01-02 16:30:44 -0500

    added validation of credential to contextProvider

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -15,3 +15,14 @@ export const loginUser = async (email, password) => {
     const data = await response.json();
     return data;
 };
+
+export const checkAuthStatus = async () => {
+    const response = await fetch(`${baseUrl}/user/auth-status`, {
+        credentials: "include",
+    });
+    if (!response.ok) {
+        throw new Error("Unable to check status");
+    }
+    const data = await response.json();
+    return data;
+};
--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
-import { loginUser } from "../api/authApi";
+import { loginUser, checkAuthStatus } from "../api/authApi";
 
 const AuthContextProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        async function checkLoginStatus() {
+            try {
+                const data = await checkAuthStatus();
+                setUser({ email: data.email, name: data.name });
+                setIsLoggedIn(true);
+            } catch (error) {
+                throw new Error("Cannot login");
+            }
+        }
+        checkLoginStatus();
+    }, []);
 
     // perform call to login api
     const login = async (email, password) => {
