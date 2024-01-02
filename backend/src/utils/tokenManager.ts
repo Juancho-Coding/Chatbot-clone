fadbff5f6ab7b8a7ca80bb13105b0385035391a8
Juancho-Coding <juanpabloalvarezovalle@gmail.com>
2024-01-02 10:44:38 -0500

    Fix token expiration and added documentation

--- backend/src/utils/tokenManager.ts
+++ backend/src/utils/tokenManager.ts
@@ -1,9 +1,21 @@
 import jwt from "jsonwebtoken";
 
+/**
+ * Generates a jwt, the payload is the user id, email
+ * @param id identification of the user
+ * @param email email of the user
+ * @param expiresIn expiration time
+ * @returns json web token
+ */
 export function tokenGeneration(id: string, email: string, expiresIn: string): string {
-    return jwt.sign({ id, email }, process.env.JWT_SECRET!, { expiresIn: "1h" });
+    return jwt.sign({ id, email }, process.env.JWT_SECRET!, { expiresIn: expiresIn });
 }
 
+/**
+ * verifies the token received was generated from this server
+ * @param token teken received
+ * @returns token was verified?
+ */
 export function tokenVerification(token: string): boolean {
     try {
         jwt.verify(token, process.env.JWT_SECRET!);
