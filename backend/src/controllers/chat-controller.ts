c00ae281289fe7d15fc15ccf6fb92e6464c49c11
Juancho-Coding <juanpabloalvarezovalle@gmail.com>
2024-01-05 23:21:55 -0500

    Added controllers to get the messages and clear all messages from DB

--- backend/src/controllers/chat-controller.ts
+++ backend/src/controllers/chat-controller.ts
@@ -5,6 +5,9 @@ import { checkValidationErrors } from "../validators/utilities";
 import { User } from "../models/user";
 import GenError from "../utils/generalError";
 
+/**
+ * Create a new mwssage, communicate with openai, save the messages and return new message
+ */
 export async function newChatCompletion(req: Request, res: Response, next: NextFunction) {
     const message: string = req.body.message;
     const userId: string = res.locals.jwtData.id as string;
@@ -17,26 +20,84 @@ export async function newChatCompletion(req: Request, res: Response, next: NextF
             const error = new GenError("User not found", 401); // user not found
             throw error;
         }
+        // format the stored chats to send them to openai
         const chats = foundUser.chats.map((chat) => {
             return { role: chat.role, content: chat.content };
         }) as OpenAI.Chat.Completions.ChatCompletionMessageParam[];
         const newChat = { role: "user", content: message };
+        // add new message to existing ones to be sent to openai
         chats.push(newChat as OpenAI.Chat.Completions.ChatCompletionUserMessageParam);
         foundUser.chats.push(newChat);
 
+        // create object to communicate with openai
         const openai = new OpenAI({
             apiKey: process.env.OPEN_AI_SECRET,
             organization: process.env.OPENAI_ORGANIZATIONID,
         });
+        // Send the messages to openai and wait response of the new message
         const chatResponse = await openai.chat.completions.create({
             messages: chats,
             model: "gpt-3.5-turbo",
-            max_tokens: 50,
         });
+        // Get the response and store it
         foundUser.chats.push(chatResponse.choices[0].message);
         chats.push(chatResponse.choices[0].message);
         await foundUser.save();
         return res.status(200).json({ chatData: chats });
+    } catch (err) {
+        if (err instanceof GenError) {
+            return next(err);
+        }
+        const error = err as openAIError;
+        return next(new GenError(`An error ocurred: ${error.message}`, 500));
+    }
+}
+
+/**
+ * Return the messages of the user
+ */
+export async function getChatMessages(req: Request, res: Response, next: NextFunction) {
+    const userId: string = res.locals.jwtData.id as string;
+    // check for validation errors
+    if (checkValidationErrors(req, res, 422)) return;
+    // no error, continue adding new chat
+    try {
+        let foundUser = await User.findById(userId);
+        if (!foundUser) {
+            const error = new GenError("User not found", 401); // user not found
+            throw error;
+        }
+        // get the chats and format the messages to be sent
+        const chats = foundUser.chats.map((chat) => {
+            return { role: chat.role, content: chat.content };
+        }) as OpenAI.Chat.Completions.ChatCompletionMessageParam[];
+        return res.status(200).json({ chatData: chats });
+    } catch (err) {
+        if (err instanceof GenError) {
+            return next(err);
+        }
+        const error = err as openAIError;
+        return next(new GenError(`An error ocurred: ${error.message}`, 500));
+    }
+}
+
+/**
+ * Clear the chats of the user
+ */
+export async function clearChatCompletion(req: Request, res: Response, next: NextFunction) {
+    const userId: string = res.locals.jwtData.id as string;
+    // check for validation errors
+    if (checkValidationErrors(req, res, 422)) return;
+    // no error, continue clearing chat
+    try {
+        let foundUser = await User.findById(userId);
+        if (!foundUser) {
+            const error = new GenError("User not found", 401); // user not found
+            throw error;
+        }
+        // update the user by removing the chats from the db
+        await User.findOneAndUpdate({ _id: userId }, { $set: { chats: [] } });
+        return res.status(200).json({ message: "ok" });
     } catch (err) {
         if (err instanceof GenError) {
             return next(err);
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -3,8 +3,14 @@ import { Router } from "express";
 import { newChatValidation } from "../validators/chatValidators";
 import { tokenValidation } from "../validators/authValidators";
 
-import { newChatCompletion } from "../controllers/chat-controller";
+import {
+    newChatCompletion,
+    clearChatCompletion,
+    getChatMessages,
+} from "../controllers/chat-controller";
 
 export const chatRoutes = Router();
 
 chatRoutes.post("/new", tokenValidation, newChatValidation, newChatCompletion);
+chatRoutes.post("/clear", tokenValidation, clearChatCompletion);
+chatRoutes.get("/get", tokenValidation, getChatMessages);
